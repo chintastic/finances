ba7793d4e0608c11e56642c9c094b0fef77b1851
chintastic <40698852+chintastic@users.noreply.github.com>
2023-03-22 19:57:13 -0400

    Made changes to accept data properly in create_tables file

--- backend/migrations/create_tables.sql
+++ backend/migrations/create_tables.sql
@@ -1,25 +1,14 @@
-DROP TABLE IF EXISTS venues;
-DROP TABLE IF EXISTS locations;
-DROP TABLE IF EXISTS categories;
-DROP TABLE IF EXISTS venue_categories;
-
-CREATE TABLE IF NOT EXISTS venues (
-  id serial PRIMARY KEY,
-  foursquare_id VARCHAR(255) UNIQUE,
-  name VARCHAR(255) NOT NULL,
-  price INTEGER,
-  rating DECIMAL,
-  likes BIGINT,
-menu_url VARCHAR(255),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE INDEX IF NOT EXISTS venues_price_index ON venues (price);
+DROP TABLE IF EXISTS transactions;
 
 
-CREATE TABLE IF NOT EXISTS categories (
-  id serial PRIMARY KEY,
-  name VARCHAR(255) UNIQUE
+CREATE TABLE IF NOT EXISTS transactions(
+  transaction_date DATE,
+  new_description VARCHAR(255),
+  original_description VARCHAR(255),
+  amount DECIMAL,
+  transaction_type VARCHAR(255),
+  category VARCHAR(255),
+  account_name VARCHAR(255)
 );
 
 
